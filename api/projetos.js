8e40cc14465d174d5b8056ed0961ac4ecb7bec1a
theXArcano <gabrielmouta06032001@gmail.com>
2022-11-02 21:41:20 -0300

    fix: tentativa include

--- api/projetos.js
+++ api/projetos.js
@@ -5,7 +5,7 @@ const prisma = new PrismaClient.PrismaClient()
 
 
 app.get('/', async (req, res) => {
-    const projetos = await prisma.projetos.findMany({include: {demos, showUp}});
+    const projetos = await prisma.projetos.findMany({include: {demos: true, showUp: true}});
     res.send(projetos);
 })
 
