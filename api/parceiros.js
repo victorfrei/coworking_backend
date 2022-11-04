79d1ee92a89e5bfe33f3c55626c332a6abf98c42
theXArcano <gabrielmouta06032001@gmail.com>
2022-11-03 21:14:35 -0300

    fix: mudança de src para url

--- api/parceiros.js
+++ api/parceiros.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express.Router()
-const PrismaClient = require('@prisma/client')
-const prisma = new PrismaClient.PrismaClient()
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
 app.get('/', async (req, res) => {
     const parceiros = await prisma.parceiros.findMany();
