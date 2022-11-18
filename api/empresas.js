eb21d8820251fda2547cdcbff611722e24162de1
victorfrei <vic.frei2@gmail.com>
2022-11-18 08:52:41 -0300

    Fix: API fix's

--- api/empresas.js
+++ api/empresas.js
@@ -4,23 +4,23 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 app.get('/', async (req, res) => {
-    const empresas = await prisma.empresa.findMany({ include: { projetos: true, parceiros: true } })
+    const empresas = await prisma.empresas.findMany({ include: { projetos: true, parceiros: true } })
     res.send(empresas);
 })
 
 app.post('/', async (req, res) => {
-    const empresas = await prisma.empresa.create({ data: req.body });
+    const empresas = await prisma.empresas.create({ data: req.body });
     res.send(empresas);
 })
 
 app.put('/:id', async (req, res) => {
-    const empresas = await prisma.empresa.update({ where: { id: req.params.id }, data: req.body });
+    const empresas = await prisma.empresas.update({ where: { id: req.params.id }, data: req.body });
     res.send(empresas);
 
 })
 
 app.delete('/:id', async (req, res) => {
-    const empresas = await prisma.empresa.delete({ where: { id: req.params.id } });
+    const empresas = await prisma.empresas.delete({ where: { id: req.params.id } });
     res.send(`A empresa com o id = ${req.params.id} foi deletado com sucesso! `)
 })
 
